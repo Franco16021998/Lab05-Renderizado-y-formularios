8173e180cd6269f218f718e01f158a51060c5a21
unknown <franco.starke@tecsup.edu.pe>
2021-04-16 18:35:56 -0500

    Varios cursos

--- cursos/src/index.js
+++ cursos/src/index.js
@@ -2,30 +2,64 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Course from "./Components/Course";
+import Header from "./Components/Header";
 
 const App = () => {
-  const course = {
-      id: 1,
+  const courses = [
+    {
       name: 'Half Stack application development',
+      id: 1,
       parts: [
-          {
-              name: 'Fundamentals of React',
-              exercises: 10,
-              id: 1
-          },
-          {
-              name: 'Using props to pass data',
-              exercises: 7,
-              id: 2
-          },
-          {
-              name: 'State of a component',
-              exercises: 14,
-              id: 3
-          }
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
       ]
-  }
-  return <Course course={course} />
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return (
+    <div>
+      <Header>
+        
+      </Header>
+      {courses.map((course, index) => {
+        return <Course key={course.id} course={course} />;
+      })}
+    </div>
+  )
 }
 
-ReactDOM.render(<App />, document.getElementById('root')) 
+ReactDOM.render(<App />, document.getElementById("root"));
